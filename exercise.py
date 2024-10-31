d2cb38da5d630fbe4a0f4360a3f388fd343150a2
Hfairbanks <Hydeia.fairbanks9620@gmail.com>
2024-10-31 11:16:18 -0400

    first commit

---
+++ exercise.py
@@ -0,0 +1,82 @@
+full_name = "john Doe"
+print (full_name)
+
+myname = "John"
+myname = "Doe"
+print(myname)
+print(myname)
+fav_color = "Blue"
+print (fav_color)
+
+fruit1, fruit2, fruit3 = "apple", "banana", "cherry"
+print(fruit1)
+print(fruit2)
+print(fruit3)
+
+integer_var = 42
+float_var = 3.14
+string_var = "Hello world"
+
+print(integer_var,type (integer_var))
+print(float_var,type (float_var))
+print(string_var,type (string_var))
+
+mystery_variable = "1234"
+variable_type = (mystery_variable)
+print(variable_type)
+
+dynamicvar =10
+print(dynamicvar, type (dynamicvar))
+
+dynamicvar = "python is great"
+print(dynamicvar, type(dynamicvar))
+
+
+flour_per_cake = 250 
+total_flour = 2.5 * 1000
+number_of_cakes = total_flour // flour_per_cake
+print(number_of_cakes)
+
+kingdom = "pythonland"
+print(kingdom)
+
+shirt1 =45
+shirt2 =50
+
+result = shirt1 < shirt2
+print(result)
+
+rain = True
+heavy_rain = False
+take_umbrela = rain or heavy_rain
+print(take_umbrela)
+
+result = 3 + 5 * 2 - 8
+print(result)
+
+
+pastries = 10
+friends = 3
+pastries_each = pastries // friends
+left_over = pastries % friends
+print(pastries_each, left_over)
+
+kingdom = "pythonland"
+kingdom += " is  wonderful" # kingdom + "is wonderful!"
+print(kingdom)
+
+knight1 = 45
+kinght2 = 50
+result = (knight1 == kinght2)
+print(result)
+
+eggs = True
+flour = False
+make_pancakes = eggs and flour
+print(make_pancakes)
+
+castle_height = 100
+moat_width = 50
+castle_height = 2 # castle_height = 200
+moat_width /= 2 # moat_width = moat-width / 2
+print(castle_height, moat_width)
