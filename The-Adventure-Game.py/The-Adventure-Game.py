684fd07d9d9c7d79916bdd5dd742e08230ebbea4
Hfairbanks <hydeia.fairbanks9620@gmail.com>
2025-01-02 13:29:57 +0000

    first commit

---
+++ The-Adventure-Game.py/The-Adventure-Game.py
@@ -0,0 +1,15 @@
+place = input("Choose a place: forest or cave? ")
+
+if place == "forest":
+    action = input("climb a tree or cross a river?")
+    if action == "climb a tree":
+        print("You found a bird's nest!")
+    elif action == "cross a river": 
+        print("You found a boat!") 
+elif place == "cave":
+    print("You find a hidden treasure!")
+elif place == "cave":
+    print("light a torch")
+elif place == "forest":
+    print("proceed in the dark")
+    
