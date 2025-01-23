8fc94f4004038dc8b2e2d3929e9bde075ec8b9d7
Hfairbanks <hydeia.fairbanks9620@gmail.com>
2025-01-23 13:38:10 +0000

    first commit

---
+++ The-event-planner.py
@@ -0,0 +1,3 @@
+attendance = (input("Enter number of attendances: "))
+venue = "large hall" if attendance > 100 else "conference room"
+print(venue)
---
+++ python list transformation.py/python list.py
@@ -0,0 +1,25 @@
+Grades= [95, 93, 90, 89, 88, 85, 80, 78, 76, 72]
+Average= 85
+
+grades = [ "95, 93, 90, 89 88, 85, 80, 78, 76, 72"]
+print( "average grade = 85" )
+
+
+submitted = [ "alice,""bob," "charlie","david,"]
+attended = ["charlie," "eve," "alice," "frankie"]
+
+if "Alice" in submitted and "alice in attended":
+     print( "Alice in submitted their submitted and alice in attended class.")
+else:
+     print("Alice did submit their submitted or did atteneded class.")
+     
+
+     temperatures= [72, 75, 78, 79, 80, 81, 82, 83, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 103,104, 105, 106]
+ 
+     print(temperatures [7:14])
+
+
+     temperatures=[72, 75, 78, 79, 80, 81, 82, 83, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 103, 104, 105, 106]
+print(temperatures[22:26])
+     
+
